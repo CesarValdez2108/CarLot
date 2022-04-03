b6b91b60d3119c387ed1f4a174ec14cd0f64b46a
CesarValdez2108 <85514996+CesarValdez2108@users.noreply.github.com>
2022-04-03 13:32:04 -0700

    Add files via upload

---
+++ CarLotProject-master/README.md
@@ -0,0 +1,3 @@
+# CarLotProject
+Nothing interesting here, just a small project for my CS course.
+This is  a second semester project, there is literally nothing of interest here(only rushed code)
---
+++ CarLotProject-master/app/src/main/java/DataManager/Vehicle.java
@@ -0,0 +1,71 @@
+package DataManager;
+import DataManager.Pool.*;
+
+public class Vehicle extends Component{
+    //TODO: Add fields and get/set methods
+    private String vehicleName = new String();
+    private Boolean quemacocos = false;
+    private Integer numPuertas = 0;
+    private Integer asientos = 0;
+    private Double kilometraje = 0.0;
+    private Double gasolina = 0.0;
+    
+    public void bindToModel(Model modelObj) throws ComponentNotBoundToPool{
+        if(!ModelPool.get().componentIsRegisteredAtPool(modelObj)){
+            throw new ComponentNotBoundToPool("The 'model' object passed to bindToModel does not exist inside ModelPool");
+        }
+        modelObj.addChild(this);
+    }
+    
+    public Model getModel(){
+        return (Model)getParent();
+    }
+
+    public String getVehicleName(){
+        return this.vehicleName;
+    }
+    
+    public void setVehicleName(String name){
+        this.vehicleName = name;
+    }
+
+    public Boolean getQuemacocos(){
+        return this.quemacocos;
+    }
+
+    public void setQuemacocos(Boolean cocos){
+        this.quemacocos = cocos;
+    }
+
+    public Integer getNumPuertas(){
+        return numPuertas;
+    }
+
+    public void setNumPuertas(Integer puertas){
+        this.numPuertas = puertas;
+    }
+
+    public Integer getAsientos(){
+        return asientos;
+    }
+
+    public void setAsientos(Integer asientos){
+        this.asientos = asientos;
+    }
+
+    public Double getKilometraje(){
+        return kilometraje;
+    }
+
+    public void setKilometraje(Double kilometraje){
+        this.kilometraje = kilometraje;
+    }
+
+    public Double getGasolina(){
+        return gasolina;
+    }
+
+    public void setGasolina(Double gasolina){
+        this.gasolina = gasolina;
+    }
+}
