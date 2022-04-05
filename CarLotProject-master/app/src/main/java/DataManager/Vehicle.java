31b9805215b97b4b2f5dabcf01134987e232d489
CesarValdez2108 <85514996+CesarValdez2108@users.noreply.github.com>
2022-04-05 00:00:53 -0700

    Update Vehicle.java
    Vehicle characteristics, also added get/set for each characteristics

--- CarLotProject-master/app/src/main/java/DataManager/Vehicle.java
+++ CarLotProject-master/app/src/main/java/DataManager/Vehicle.java
@@ -1,71 +1,75 @@
 package DataManager;
+
 import DataManager.Pool.*;
 
-public class Vehicle extends Component{
-    //TODO: Add fields and get/set methods
+public class Vehicle extends Component {
+    // TODO: Add fields and get/set methods
     private String vehicleName = new String();
     private Boolean quemacocos = false;
     private Integer numPuertas = 0;
     private Integer asientos = 0;
     private Double kilometraje = 0.0;
     private Double gasolina = 0.0;
-    
-    public void bindToModel(Model modelObj) throws ComponentNotBoundToPool{
-        if(!ModelPool.get().componentIsRegisteredAtPool(modelObj)){
-            throw new ComponentNotBoundToPool("The 'model' object passed to bindToModel does not exist inside ModelPool");
+
+    public Vehicle(Model model) throws ComponentNotBoundToPool {
+        if (model == null)
+            ;
+        if (!ModelPool.get().componentIsRegisteredAtPool(model)) {
+            throw new ComponentNotBoundToPool(
+                    "The 'model' object passed to bindToModel does not exist inside ModelPool");
         }
-        modelObj.addChild(this);
-    }
-    
-    public Model getModel(){
-        return (Model)getParent();
+        model.addChild(this);
     }
 
-    public String getVehicleName(){
+    public Model getModel() {
+        return (Model) getParent();
+    }
+
+    public String getVehicleName() {
         return this.vehicleName;
     }
-    
-    public void setVehicleName(String name){
+
+    public void setVehicleName(String name) {
         this.vehicleName = name;
     }
 
-    public Boolean getQuemacocos(){
+    public Boolean getQuemacocos() {
         return this.quemacocos;
     }
 
-    public void setQuemacocos(Boolean cocos){
+    public void setQuemacocos(Boolean cocos) {
         this.quemacocos = cocos;
     }
 
-    public Integer getNumPuertas(){
+    public Integer getNumPuertas() {
         return numPuertas;
     }
 
-    public void setNumPuertas(Integer puertas){
+    public void setNumPuertas(Integer puertas) {
         this.numPuertas = puertas;
     }
 
-    public Integer getAsientos(){
+    public Integer getAsientos() {
         return asientos;
     }
 
-    public void setAsientos(Integer asientos){
+    public void setAsientos(Integer asientos) {
         this.asientos = asientos;
     }
 
-    public Double getKilometraje(){
+    public Double getKilometraje() {
         return kilometraje;
     }
 
-    public void setKilometraje(Double kilometraje){
+    public void setKilometraje(Double kilometraje) {
         this.kilometraje = kilometraje;
     }
 
-    public Double getGasolina(){
+    public Double getGasolina() {
         return gasolina;
     }
 
-    public void setGasolina(Double gasolina){
+    public void setGasolina(Double gasolina) {
         this.gasolina = gasolina;
     }
 }
